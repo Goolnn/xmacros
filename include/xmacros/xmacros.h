616bd24936316c1aaaa92fbc93059ecb7fbf3a64
Goolnn <Goolnn@hotmail.com>
2025-04-30 17:43:34 +0800

    feat: implement join macro

--- include/xmacros/xmacros.h
+++ include/xmacros/xmacros.h
@@ -4,6 +4,7 @@
 #include "xmacros/priv/overload.h"
 #include "xmacros/priv/combine.h"
 #include "xmacros/priv/concat.h"
+#include "xmacros/priv/join.h"
 #include "xmacros/priv/argc.h"
 #include "xmacros/priv/argn.h"
 
@@ -12,6 +13,7 @@
 
 #define xmacros_combine(...) priv_combine(__VA_ARGS__)
 #define xmacros_concat(...) priv_concat(__VA_ARGS__)
+#define xmacros_join(...) priv_join(__VA_ARGS__)
 
 #define xmacros_overload(p, q) priv_overload(p, q)
 
