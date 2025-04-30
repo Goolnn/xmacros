0b9b5d7f1ac098f62181452e884d4abd75c992b6
Goolnn <Goolnn@hotmail.com>
2025-04-30 17:36:40 +0800

    feat: implement last private utility macro

--- include/xmacros/priv/utils.h
+++ include/xmacros/priv/utils.h
@@ -1,11 +1,13 @@
 #ifndef GOOLNN_XMACROS_PRIV_UTILS_H
 #define GOOLNN_XMACROS_PRIV_UTILS_H
 
+#include "xmacros/priv/utils/reverse.h"
 #include "xmacros/priv/utils/pop.h"
 
 #define wrap(...) (__VA_ARGS__)
 #define unwrap(...) __VA_ARGS__
 
 #define first(v, ...) v
+#define last(...) unwrap(first wrap(priv_reverse(__VA_ARGS__)))
 
 #endif
