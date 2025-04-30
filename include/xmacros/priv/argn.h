e13be27eb3e0bda395472d6040e6a5c2e7b97ae1
Goolnn <Goolnn@hotmail.com>
2025-04-30 17:11:49 +0800

    feat: implement argn macro

---
+++ include/xmacros/priv/argn.h
@@ -0,0 +1,8 @@
+#ifndef GOOLNN_XMACROS_PRIV_ARGN_H
+#define GOOLNN_XMACROS_PRIV_ARGN_H
+
+#include "xmacros/priv/utils.h"
+
+#define priv_argn(n, ...) unwrap(first wrap(priv_pop(n, __VA_ARGS__)))
+
+#endif
--- include/xmacros/xmacros.h
+++ include/xmacros/xmacros.h
@@ -2,7 +2,9 @@
 #define GOOLNN_XMACROS_XMACROS_H
 
 #include "xmacros/priv/argc.h"
+#include "xmacros/priv/argn.h"
 
 #define xmacros_argc(...) priv_argc(__VA_ARGS__)
+#define xmacros_argn(n, ...) priv_argn(n, __VA_ARGS__)
 
 #endif
