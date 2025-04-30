0c7eba118f99dec5ea48b5ab38998235ad4e8e3e
Goolnn <Goolnn@hotmail.com>
2025-04-30 18:23:53 +0800

    feat: implement suffix macros

--- include/xmacros/xmacros.h
+++ include/xmacros/xmacros.h
@@ -3,6 +3,7 @@
 
 #include "xmacros/priv/overload.h"
 #include "xmacros/priv/combine.h"
+#include "xmacros/priv/suffix.h"
 #include "xmacros/priv/prefix.h"
 #include "xmacros/priv/concat.h"
 #include "xmacros/priv/join.h"
@@ -21,5 +22,9 @@
 #define xmacros_prefix_combine(pre, ...) priv_prefix_combine(pre, __VA_ARGS__)
 #define xmacros_prefix_concat(pre, ...) priv_prefix_concat(pre, __VA_ARGS__)
 #define xmacros_prefix_join(pre, sep, ...) priv_prefix_join(pre, sep, __VA_ARGS__)
+
+#define xmacros_suffix_combine(pre, ...) priv_suffix_combine(pre, __VA_ARGS__)
+#define xmacros_suffix_concat(pre, ...) priv_suffix_concat(pre, __VA_ARGS__)
+#define xmacros_suffix_join(pre, sep, ...) priv_suffix_join(pre, sep, __VA_ARGS__)
 
 #endif
