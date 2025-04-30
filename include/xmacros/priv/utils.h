1092370f3fb4e06ab4f4a7f37f7dafba0ccdf0d3
Goolnn <Goolnn@hotmail.com>
2025-04-30 17:11:38 +0800

    feat: implement wraps and first private utility macros

---
+++ include/xmacros/priv/utils.h
@@ -0,0 +1,11 @@
+#ifndef GOOLNN_XMACROS_PRIV_UTILS_H
+#define GOOLNN_XMACROS_PRIV_UTILS_H
+
+#include "xmacros/priv/utils/pop.h"
+
+#define wrap(...) (__VA_ARGS__)
+#define unwrap(...) __VA_ARGS__
+
+#define first(v, ...) v
+
+#endif
