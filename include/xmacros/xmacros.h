1603084f29657d4861738e6e5bba7df09d49e802
Goolnn <Goolnn@hotmail.com>
2025-04-30 17:40:12 +0800

    feat: implement concat macro

--- include/xmacros/xmacros.h
+++ include/xmacros/xmacros.h
@@ -2,6 +2,8 @@
 #define GOOLNN_XMACROS_XMACROS_H
 
 #include "xmacros/priv/overload.h"
+#include "xmacros/priv/combine.h"
+#include "xmacros/priv/concat.h"
 #include "xmacros/priv/argc.h"
 #include "xmacros/priv/argn.h"
 
@@ -9,6 +11,7 @@
 #define xmacros_argn(n, ...) priv_argn(n, __VA_ARGS__)
 
 #define xmacros_combine(...) priv_combine(__VA_ARGS__)
+#define xmacros_concat(...) priv_concat(__VA_ARGS__)
 
 #define xmacros_overload(p, q) priv_overload(p, q)
 
